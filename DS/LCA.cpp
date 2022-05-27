f983cdf9e8e1733a00fceab73db05591136727e2
Md Sabbir Rahman <red.abiir@gmail.com>
2022-05-27 20:49:02 +0600

    minor changes

--- DS/LCA.cpp
+++ DS/LCA.cpp
@@ -3,7 +3,8 @@ int anc[N][K]; int level[N];
 void setup(int u, int par) {
   level[u] = level[par]+1;
   anc[u][0] = par;
-  for (int k=1; k<K; k++)   anc[u][k] = anc[anc[u][k-1]][k-1];
+  for (int k=1; k<K; k++)
+    anc[u][k] = anc[anc[u][k-1]][k-1];
   for (int v: adj[u]) {
     if (v == par)   continue;
     setup(v, u);
@@ -12,7 +13,8 @@ void setup(int u, int par) {
 int lca(int u, int v) {
   if (level[u] > level[v])  swap(u, v);
   for (int k=K-1; k>=0; k--)
-    if (level[u] + (1<<k) <= level[v])  v = anc[v][k];
+    if (level[u] + (1<<k) <= level[v])
+      v = anc[v][k];
   if (u == v) return u;
   for (int k=K-1; k>=0; k--)
     if (anc[u][k] != anc[v][k])
@@ -24,8 +26,4 @@ int getanc(int u, int d) {
     if (d & (1<<k))
       u = anc[u][k];
   return u;
-}
-int dist(int u, int v) {
-  int g = lca(u, v);
-  return level[u] + level[v] - 2*level[g];
 }
--- DS/Sparsetable.cpp
+++ DS/Sparsetable.cpp
@@ -1,12 +1,13 @@
-int K[N][int(log2(N))+1], A[N];
+int table[N][int(log2(N))+1], A[N];
 void buildSparseforMIN(int n){
-  for(int i=1;i<=n;i++) K[i][0]=A[i];
+  for(int i=1;i<=n;i++) table[i][0]=A[i];
   for(int j=1; (1<<j)<=n ;j++)
     for(int i=1; (i+(1<<j)-1)<=n; i++)
-      K[i][j]=min(K[i][j-1],
-          K[i+(1<<(j-1))][j-1]);
+      table[i][j]=min(table[i][j-1],
+          table[i+(1<<(j-1))][j-1]);
 }
 int MIN(int i,int j){
+  //int k = 32 - __builtin_clz(j-i+1) - 1;
   int k=log2(j-i+1);
-  return min(K[i][k],K[j-(1<<k)+1][k]);
+  return min(table[i][k],table[j-(1<<k)+1][k]);
 }
